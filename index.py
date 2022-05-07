01458ea697314a5a896bbabff34de037bccf92bf
Ahmed-Elghrbawy <ahmed@codecov.io>
2022-05-07 17:15:53 +0200

    testo

---
+++ index.py
@@ -0,0 +1,14 @@
+
+def func(x):
+    if x < 3:
+        return True
+    
+    return False
+
+
+def other_func(z):
+    x = 0
+    for i in range(z + 1):
+        x += i
+        
+    return x    
